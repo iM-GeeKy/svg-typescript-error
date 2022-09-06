de7e1733f0a1e306f2d8363a53e0d4da003c6ce6
Brandon Fitzwater <brandonfitzh2o@gmail.com>
2022-09-06 16:40:24 -0400

    Create example typescript expo project.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Svg, { G, Rect } from 'react-native-svg';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Svg height="20" width="90%">
+        <G>
+          <Rect
+            x="0"
+            fill={'orange'}
+            y="10"
+            width="200"
+            height="200"
+          />
+        </G>
+      </Svg>
     </View>
   );
 }
